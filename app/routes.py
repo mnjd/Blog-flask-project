0b1b5e183748ef3fc79ab473af1ae438b25d3016
manojd <manoj.ducher@gmail.com>
2018-02-12 20:35:07 +0100

    modified routes for heroku

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import psycopg2
+import os
 
 #POSTGRES = {
 #    'user': 'postgres',
