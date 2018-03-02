c8b3a9afdb52633e722217318cd56043450e176c
manojd <manoj.ducher@gmail.com>
2018-03-02 17:15:59 +0100

    added user registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from datetime import datetime
+from wtforms import Form, TextField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from sqlalchemy.orm import sessionmaker
 import psycopg2
 import os
 
@@ -14,10 +18,19 @@ POSTGRES = {
 
 app = Flask(__name__) # create the application instance
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES # load config from this file
-
+app.config['SECRET_KEY']='Thisisthesecretkey'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
+
+login = LoginManager(app)
+
+
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=20)])
+    password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm', message="Password must match")])
+    confirm = PasswordField('Repeat password')
+    email = TextField('Email Adress', [validators.Length(min=6,max=50)])
 
 
 class Postblog(db.Model):
@@ -28,7 +41,11 @@ class Postblog(db.Model):
     text = db.Column(db.Text)
     created_at = db.Column(db.DateTime)
 
-
+class User(db.Model):
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.Text)
+    email = db.Column(db.String(50))
+    
 @app.route("/")
 def list_articles():
     posts = Postblog.query.all()
@@ -87,6 +104,36 @@ def delete_post(id):
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for('list_articles'))
+
+@app.route("/login/", methods = ['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        attempted_username = request.form['username']
+        attempted_password = request.form['password']
+        
+        query = db.session.query(User).filter(User.username.in_([attempted_username]), User.password.in_([attempted_password]))
+        result = query.first()
+        if result:
+            login(attempted_username)
+            return redirect(url_for('list_articles'))
+        else:
+            flash('wrong password!')
+    return render_template('login.html')
+
+@app.route("/register/", methods = ['GET','POST'])
+def registration_page():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = sha256_crypt.encrypt(request.form['password'])
+
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('list_articles'))
+
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
